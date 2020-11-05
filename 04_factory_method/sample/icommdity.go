9663e212122d0618f1758920405d70039cd1af5a
Rocket, Zhao <rocket.zhao@hp.com>
2020-11-05 15:27:07 +0800

    feat(designPattern): the sample of factory method

--- 04_factory_method/sample/icommdity.go
+++ 04_factory_method/sample/icommdity.go
@@ -1,5 +1,11 @@
 package sample
 
+/**
+	模拟发放多种奖品
+	- 1.优惠券
+	- 2.实物商品
+	- 3.爱奇艺的兑换卡
+ */
 type ICommodity interface {
 	SendCommodity(uId string, commodityId string, bizId string, Map map[string]string) error
 }
