9f8e9e89add4be147837f0de2e97663c91053f59
rocket.zhao <rocket.zhao@hp.com>
2021-10-21 16:20:11 +0800

    feat(adapter): add comment

--- 02_adapter/classadpter_standard/class_adapter.go
+++ 02_adapter/classadpter_standard/class_adapter.go
@@ -4,7 +4,9 @@ package classadpter_standard
 适配器
 */
 type ClassAdapter struct {
-	// 继承
+	//如果一个struct嵌套了另一个匿名结构体，那么这个结构可以直接访问匿名结构体的方法，从而实现继承
+	//如果一个struct嵌套了另一个【有名】的结构体，那么这个模式叫做组合
+	//如果一个struct嵌套了多个匿名结构体，那么这个结构可以直接访问多个匿名结构体的方法，从而实现多重继承
 	Adaptee
 }
 
